7f76af287502d56e9125b2bece9c28c38dcc64f0
Evelyn Gaitan <143912123+evelyngaitan92@users.noreply.github.com>
2024-07-05 00:09:36 -0300

    Se agrega login html

---
+++ backend/alta-usuarios.py
@@ -0,0 +1,40 @@
+
+
+from conexion import *
+
+class Catalogo:
+    
+    # Constructor de la clase
+    def __init__(self, host, user, password, database):
+        # Primero, establecemos una conexión sin especificar la base de datos
+        self.conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password
+        )
+        self.cursor = self.conn.cursor()
+
+        # Intentamos seleccionar la base de datos
+        try:
+            self.cursor.execute(f"USE {database}")
+        except mysql.connector.Error as err:
+            # Si la base de datos no existe, la creamos
+            if err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+                self.cursor.execute(f"CREATE DATABASE {database}")
+                self.conn.database = database
+            else:
+                raise err
+
+        # Una vez que la base de datos está establecida, creamos la tabla si no existe
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
+            codigo INT AUTO_INCREMENT PRIMARY KEY,
+            usuario VARCHAR(255) NOT NULL,
+            contraseña VARCHAR(255) NOT NULL,
+            nombre VARCHAR(255) NOT NULL,
+            apellido VARCHAR(255) NOT NULL,
+            familia VARCHAR(255) NOT NULL ''')
+        self.conn.commit()
+
+        # Cerrar el cursor inicial y abrir uno nuevo con el parámetro dictionary=True
+        self.cursor.close()
+        self.cursor = self.conn.cursor(dictionary=True)
---
+++ backend/conexion.py
@@ -0,0 +1,27 @@
+#Archivo para hacer la conexion. 
+
+# Instalar con pip install Flask
+from flask import Flask, request, jsonify, render_template
+
+# Instalar con pip install flask-cors
+from flask_cors import CORS
+
+# Instalar con pip install mysql-connector-python
+import mysql.connector
+
+# Si es necesario, pip install Werkzeug
+from werkzeug.utils import secure_filename
+
+# No es necesario instalar, es parte del sistema standard de Python
+import os
+import time
+#--------------------------------------------------------------------
+#variables para configurar la base de datos.
+HOST = 'evelyngaitan22.mysql.pythonanywhere-services.com'
+USER = 'evelyngaitan22'
+PASSWORD = 'cac-2024#' 
+DATABASE = 'evelyngaitan22$ticket-cac'
+#HOLA = HOLA para poner todo en mayuscula seleccionar ctr + shift y el comando uppercase en la barra 
+
+app = Flask(__name__)
+CORS(app)  # Esto habilitará CORS para todas las rutas
